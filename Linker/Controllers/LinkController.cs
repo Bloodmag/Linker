c0f2b24392a1668c51ffefb59a1103ca6ba38384
Данила Золотопупов <bloodmagv@gmail.com>
2019-10-29 16:57:53 +0300

    encoding

--- Linker/Controllers/LinkController.cs
+++ Linker/Controllers/LinkController.cs
@@ -27,6 +27,8 @@ namespace Linker.Controllers
             {
                 User user = db.Users.Where(x => x.Guid == guid).First();
                 Link link = new Link();
+                0b0000000000000000000000010001100011000100001001110100010010101011
+                //max = 11 00110011 11110000 10010110 01101111 10000000
                 //link.Created
                 //user.Links.Add()
                 return Ok(Json("http://google.com"));
@@ -63,9 +65,41 @@ namespace Linker.Controllers
                     "<h1>Твоя ссылка не существует<br>" +
                     "*шепотом*лох</h1>"
                 };
-            }
+            } 
         }
-        
+
+        private static long Pow(long x, long y)
+        {
+            long ans = 1;
+            for (int i = 0; i < y; i++)
+            {
+                ans *= x;
+            }
+            return ans;
+        }
+
+        private static long? CodeToLong(string code)
+        {
+            if (code.Length != 7) return null;
+            long num = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (code[i] >= '0' && code[i] <= '9')
+                {
+                    num += (code[i] - '0') * Pow(62, 6 - i);
+                }
+                else if (code[i] >= 'A' && code[i] <= 'Z')
+                {
+                    num += (code[i] - 'A' + 10) * Pow(62, 6 - i);
+                }
+                else if (code[i] >= 'a' && code[i] <= 'z')
+                {
+                    num += (code[i] - 'a' + 36) * Pow(62, 6 - i);
+                }
+                else return null;
+            }
+            return num;
+        }
 
     }
 }
