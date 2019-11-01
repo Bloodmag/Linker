90f40736c11619c2779f5748a79f885e54e81be8
Данила Золотопупов <bloodmagv@gmail.com>
2019-11-01 16:11:03 +0300

    encoding

--- Linker/Controllers/LinkController.cs
+++ Linker/Controllers/LinkController.cs
@@ -13,6 +13,9 @@ namespace Linker.Controllers
     public class LinkController : Controller
     {
         private ApplicationContext db;
+        static char[] dict;
+        const long mask = 0b0000000000000000000000010101100011000100001001110100010010101011;
+        //lim ->        =                         110011001111110000100101100110111110000000
         public LinkController(ApplicationContext context)
         {
             db = context;
@@ -27,8 +30,8 @@ namespace Linker.Controllers
             {
                 User user = db.Users.Where(x => x.Guid == guid).First();
                 Link link = new Link();
-                0b0000000000000000000000010001100011000100001001110100010010101011
-                //max = 11 00110011 11110000 10010110 01101111 10000000
+                
+                
                 //link.Created
                 //user.Links.Add()
                 return Ok(Json("http://google.com"));
@@ -103,3 +106,70 @@ namespace Linker.Controllers
 
     }
 }
+
+/*
+ * static char[] dict;
+        const long mask = 0b0000000000000000000000010101100011000100001001110100010010101011;
+        static Program()
+        {
+            dict = new char[62];
+            for (int i = 0; i < 10; i++)
+                dict[i] = (char)('0' + i);
+            for (int i = 0; i < 26; i++)
+                dict[i+10] = (char)('A' + i);
+            for (int i = 0; i < 26; i++)
+                dict[i + 36] = (char)('a' + i);
+        }
+        private static long Pow(long x, long y)
+        {
+            long ans = 1;
+            for (int i = 0; i < y; i++)
+            {
+                ans *= x;
+            }
+            return ans;
+        }
+
+        private static string LongToCode(long num)
+        {
+            if (num < 0 || num > 3521614606207)
+                return null;
+            string ans = "";
+            num ^= mask;
+            for(int i = 6; i >= 0; i--)
+            {
+                if (num >= Pow(62, i))
+                {
+                    ans += dict[num / Pow(62, i)];
+                }
+                else
+                    ans += '0';
+                num %= Pow(62, i);
+            }
+            return ans;
+        }
+
+
+        private static long? CodeToLong(string code)
+        {
+            if (code==null || code.Length != 7) return null;
+            long num = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (code[i] >= '0' && code[i] <= '9')
+                {
+                    num += (code[i] - '0')*Pow(62, 6 - i);
+                }
+                else if (code[i] >= 'A' && code[i] <= 'Z')
+                {
+                    num += (code[i] - 'A' + 10) *Pow(62, 6 - i);
+                }
+                else if (code[i] >= 'a' && code[i] <= 'z')
+                {
+                    num += (code[i] - 'a' + 36)*Pow(62, 6 - i);
+                }
+                else return null;
+            }
+            return num^mask;
+        }
+*/
